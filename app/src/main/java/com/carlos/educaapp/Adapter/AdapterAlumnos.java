38ed7d23bba3197534deff81c2afb6c4a1e4fe92
Carlos <carlos.huaynates@tecsup.edu.pe>
2019-12-04 06:37:40 -0500

    seccion2acabada

--- app/src/main/java/com/carlos/educaapp/Adapter/AdapterAlumnos.java
+++ app/src/main/java/com/carlos/educaapp/Adapter/AdapterAlumnos.java
@@ -3,10 +3,13 @@ package com.carlos.educaapp.Adapter;
 import android.app.Dialog;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -42,7 +45,8 @@ public class AdapterAlumnos extends RecyclerView.Adapter<AdapterAlumnos.ViewHold
 
 
     @Override
-    public ViewHolderAlumnos onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    public ViewHolderAlumnos onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
+
         View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_alumnos,null,false);
         view.setOnClickListener(this);
         final ViewHolderAlumnos vHolder=new ViewHolderAlumnos(view);
@@ -55,10 +59,21 @@ public class AdapterAlumnos extends RecyclerView.Adapter<AdapterAlumnos.ViewHold
            public void onClick(View view) {
                TextView seccion=(TextView)myDialog.findViewById(R.id.idSeccionAlumno);
                TextView grado_text=(TextView)myDialog.findViewById(R.id.idGrado);
-
+               TextView correo=(TextView)myDialog.findViewById(R.id.idCorreo);
+               TextView nombre=(TextView)myDialog.findViewById(R.id.idNombreAlumno);
+               TextView apellidopaterno=(TextView)myDialog.findViewById(R.id.idApellidosPaternosAlumno);
+               TextView apellidomaterno=(TextView)myDialog.findViewById(R.id.idApellidosMaternosAlumnos);
+               TextView incidencias=(TextView)myDialog.findViewById(R.id.idNumerodeIncidencias);
                seccion.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getSeccion());
                grado_text.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getGrado());
+               correo.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getEmailPadre());
+               nombre.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getNombre());
+               incidencias.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getCantidadIncidencias().toString());
+               apellidomaterno.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getApeMaterno());
+               apellidopaterno.setText(listaAlumnos.get(vHolder.getAdapterPosition()).getApePaterno());
                 myDialog.show();
+               Window window = myDialog.getWindow();
+               window.setLayout(Toolbar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.WRAP_CONTENT);
 
                 Toast.makeText(context,"Text Click"+String.valueOf(vHolder.getAdapterPosition()),Toast.LENGTH_SHORT).show();
             }
